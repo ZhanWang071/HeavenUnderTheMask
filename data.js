b555270d228bc787c376f104e5621074b503ea3d
Zhan Wang <wz986970406@gmail.com>
2023-05-09 10:49:32 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,629 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-during-flight",
+      "name": "During Flight",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -1.025417597402182,
+        "pitch": 0.13020714526320099,
+        "fov": 1.4074385690322229
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9877494623730598,
+          "pitch": -0.12101258599482279,
+          "rotation": 4.71238898038469,
+          "target": "1-customs"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9036088624049636,
+          "pitch": 0.033452078840474186,
+          "title": "The sunshine is bright, so is your mind.",
+          "text": "Hopefully, you can meet the chief scientist himself, you are approaching the border of your research."
+        }
+      ]
+    },
+    {
+      "id": "1-customs",
+      "name": "Customs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0980099510414778,
+          "pitch": 0.14792852018713454,
+          "rotation": 0,
+          "target": "2-out-of-airport"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.057498163116552,
+          "pitch": 0.1319591074475035,
+          "title": "This is your first time to this country",
+          "text": "Everything looks different from your hometown, everything is so in order"
+        },
+        {
+          "yaw": 2.443864339109548,
+          "pitch": 0.13511947747487163,
+          "title": "Why should I take the chip?",
+          "text": "Can I refuse it?"
+        }
+      ]
+    },
+    {
+      "id": "2-out-of-airport",
+      "name": "Out of airport",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.00559227135289575,
+          "pitch": -0.13399161397322956,
+          "title": "Many soldiers around",
+          "text": "So it is true that they are preparing a war?"
+        }
+      ]
+    },
+    {
+      "id": "3-singapore_embassy",
+      "name": "Singapore_Embassy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2221865243480998,
+          "pitch": 0.02437038802709779,
+          "rotation": 0,
+          "target": "4-central-academy-of-sciences"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.081261279919433,
+          "pitch": 0.19405592391203186,
+          "title": "You got another job to do",
+          "text": "Did you really leave your duty behind? Or you just had a long holidy"
+        }
+      ]
+    },
+    {
+      "id": "4-central-academy-of-sciences",
+      "name": "Central Academy of Sciences",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2991665684583218,
+          "pitch": 0.0617440910623408,
+          "rotation": 0,
+          "target": "5-conference-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.269781407460963,
+          "pitch": -0.06674645678182856,
+          "title": "<span style=\"color: rgb(77, 81, 86); font-family: arial, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">Dmitri Ivanovich</span>",
+          "text": "The great chemist who found the Periodic Table of Elements, and the strongest reason why you choose your current road."
+        },
+        {
+          "yaw": -2.1100549154010046,
+          "pitch": -0.08131258991371837,
+          "title": "Nikolae Sokolovsky",
+          "text": "It is your honor to meet him, the current chief scientist"
+        }
+      ]
+    },
+    {
+      "id": "5-conference-room",
+      "name": "Conference Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.394059859489559,
+          "pitch": 0.002754005592201736,
+          "rotation": 0,
+          "target": "6-secret-arsenal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.006539110300019857,
+          "pitch": 0.24162668103231155,
+          "title": "What a huge room!",
+          "text": "Finally I meet Prof. Sokolovsky, but in the way I have never imagined."
+        },
+        {
+          "yaw": -3.1316059884892766,
+          "pitch": -0.4808483966338102,
+          "title": "The sunshine is bright",
+          "text": "Your mind is no more like that"
+        }
+      ]
+    },
+    {
+      "id": "6-secret-arsenal",
+      "name": "Secret Arsenal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8711724926700288,
+          "pitch": 0.13608003672292845,
+          "rotation": 4.71238898038469,
+          "target": "8-villa"
+        },
+        {
+          "yaw": 1.153396085918212,
+          "pitch": 0.1332067988456167,
+          "rotation": 1.5707963267948966,
+          "target": "9-square"
+        },
+        {
+          "yaw": 1.009918494299285,
+          "pitch": 0.13476933140677794,
+          "rotation": 0,
+          "target": "7-generals-office"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0195816735253764,
+          "pitch": 0.17296838796094427,
+          "title": "Back again",
+          "text": "Out of question, you know exactly how to assemble and disemble every weapon in the arsenal"
+        },
+        {
+          "yaw": -2.3573231432750745,
+          "pitch": -0.000689376515042639,
+          "title": "Question is",
+          "text": "does this way really protect my familly?"
+        },
+        {
+          "yaw": 1.0101775176895789,
+          "pitch": -0.04282831013981614,
+          "title": "Heavy Gate",
+          "text": "able to sustain the direct attack of even nuclear weapon, very professional."
+        }
+      ]
+    },
+    {
+      "id": "7-generals-office",
+      "name": "General's office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.03281519541251399,
+          "pitch": 0.35124922910097567,
+          "title": "<div>A magnificent office</div>",
+          "text": "Symbol of a powerful general"
+        }
+      ]
+    },
+    {
+      "id": "8-villa",
+      "name": "Villa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7846732675769683,
+          "pitch": 0.07017013976965814,
+          "rotation": 6.283185307179586,
+          "target": "12-sewer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9809194848422784,
+          "pitch": 0.2775243840388413,
+          "title": "Grand Lawn",
+          "text": "Only servers for the general himself"
+        }
+      ]
+    },
+    {
+      "id": "9-square",
+      "name": "Square",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.1124160032424584,
+          "pitch": -0.12478375564530886,
+          "title": "This is it, symbol of power",
+          "text": "Will I hesitate if I ever get a chance to give a speech there?"
+        },
+        {
+          "yaw": -0.010616755589065718,
+          "pitch": 0.10306112635042552,
+          "title": "Broad Avenue",
+          "text": "You really miss your little island, no cold air, only the scents of&nbsp;cocktail and local roasted fish."
+        },
+        {
+          "yaw": -1.5800664548289802,
+          "pitch": 0.15601033489874538,
+          "title": "You are too tired",
+          "text": "This is not what you want, but you have no choice before that."
+        }
+      ]
+    },
+    {
+      "id": "10-villas-study",
+      "name": "Villa's study",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.638217157427352,
+          "pitch": 0.27601916332434584,
+          "rotation": 10.210176124166829,
+          "target": "12-sewer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5890395604695495,
+          "pitch": -0.061463555095684796,
+          "title": "<div>Is everything worth it?</div>",
+          "text": "You just hope that your nightmare can be end ASAP."
+        }
+      ]
+    },
+    {
+      "id": "11-villas-bedroom",
+      "name": "Villa's bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7900954998143437,
+          "pitch": 0.259473637691535,
+          "rotation": 4.71238898038469,
+          "target": "12-sewer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.876476033814031,
+          "pitch": 0.4170793649485862,
+          "title": "No offense",
+          "text": "but you have to do like this"
+        }
+      ]
+    },
+    {
+      "id": "12-sewer",
+      "name": "Sewer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.580866438896951,
+          "pitch": 0.037478090379966744,
+          "rotation": 0,
+          "target": "8-villa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1346846296074826,
+          "pitch": 0.016908499314258307,
+          "title": "You can get out",
+          "text": "How about your wife and daughter?"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
